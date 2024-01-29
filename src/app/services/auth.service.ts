c42924aeac18f68251605d13ddaf72478b037c3b
Sillysally <92034603+SallyResch@users.noreply.github.com>
2024-01-29 14:02:30 +0100

    added services for authentication

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { environment } from 'src/environments/auths.service';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  baseApiUrl: string = environment.baseApiUrl;
+  constructor( private http : HttpClient) { }
+
+signUp(userObject:any){
+return this.http.post<any>(`${this.baseApiUrl}register`, userObject);
+}
+
+login(loginObject: any){
+  return this.http.post<any>(`${this.baseApiUrl}authenticate`, loginObject);
+}
+
 }
